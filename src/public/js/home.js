3454c5b3f327220c9760f2da2284211c3d015bed
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-07-03 20:45:58 +0900

    Create home.js

---
+++ src/public/js/home.js
@@ -0,0 +1,16 @@
+// const input = document.querySelector(".input__file")
+
+// async function handleSubmit(event){
+//     const file = event.srcElement.files[0]
+//     const formData = new FormData();
+//     formData.append("image", file)
+
+//     await fetch("/", {
+//         method: "POST",
+//         body: formData,
+//       }).catch(console.error);
+// }
+
+
+// input.addEventListener("change", handleSubmit)
+
