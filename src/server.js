f580ab14d06647ed7d7f128617d23cac866f2093
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-07-09 14:08:06 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from "express";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import { getJoin, postJoin, getLogin, postLogin } from "./controllers/useController";
 
@@ -12,12 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   session({
-    secret: "정준",
+    secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
+app.use(flash());
 app.route("/").get(getJoin).post(postJoin);
 app.route("/login").get(getLogin).post(postLogin)
 
