65f377e8adad4165058984a463c5ed498473060d
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-07-09 18:42:21 +0900

    Create middlewares.js

---
+++ src/middlewares.js
@@ -0,0 +1,14 @@
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+      return next();
+    } else {
+      req.flash("info", "Log in first.");
+      return res.redirect("/");
+    }
+  };
+
+export const localsMiddleware = (req, res, next) => {
+    res.locals.loggedIn = Boolean(req.session.loggedIn)
+    res.locals.loggedInUser = req.session.user || {}
+    next()
+};
