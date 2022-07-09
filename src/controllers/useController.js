d4cad0a0d3825a1a20f7f00e3786446c75f752bd
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-07-09 18:42:17 +0900

    Update useController.js

--- src/controllers/useController.js
+++ src/controllers/useController.js
@@ -1,13 +1,43 @@
 import User from "../models/User"
 import bcrypt from "bcrypt";
 
+
+// 로그인
+export const getLogin = (_, res) => res.render("home")
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({username});
+    if (!user) {
+      return res.status(400).render("home", {
+        errorMessage: "An account with this username does not exists.",
+      });
+    }
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+      return res.status(400).render("home", {
+        errorMessage: "Wrong password",
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+}
+
+// 로그 아웃
+export const logout = (req, res) => {
+  req.flash("info", "Bye Bye");
+  req.session.destroy();
+  return res.redirect("/");
+};
+
+
 // 회원 가입
 export const getJoin = (_, res)=> res.render("join")
 export const postJoin = async (req, res) => {
     const { name, username, email, password, password2} = req.body;
     // 비밀 번호가 틀렸을 경우 errorMessage를 송출.
     if (password !== password2) {
-        req.flash("error", "Password confirmation does not match.")
+        req.flash("info", "Password confirmation does not match.")
         return res.status(400).render("join", {
             errorMessage: "Password confirmation does not match.",
       });
@@ -34,23 +64,3 @@ export const postJoin = async (req, res) => {
       }
 }
 
-// 로그인
-export const getLogin = (_, res) => res.render("login")
-export const postLogin = async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({username});
-    if (!user) {
-      return res.status(400).render("login", {
-        errorMessage: "An account with this username does not exists.",
-      });
-    }
-    const ok = await bcrypt.compare(password, user.password);
-    if (!ok) {
-      return res.status(400).render("login", {
-        errorMessage: "Wrong password",
-      });
-    }
-    req.session.loggedIn = true;
-    req.session.user = user;
-    return res.send("you are logged in");
-}
